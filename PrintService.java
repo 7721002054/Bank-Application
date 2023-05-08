96c539d37ab89d826b9cd421741dd789587badcc
Gaurav <93921436+7721002054@users.noreply.github.com>
2023-05-09 00:34:08 +0530

    Add files via upload

---
+++ PrintService.java
@@ -0,0 +1,25 @@
+package com.application;
+
+public class PrintService {
+	public static void printMenu() {
+	    System.out.println("WElcome to GG Bank " + "\n" +
+	            "\r" + "1.Create Account" + "\n" +
+	            "\r" + "2.Log in" + "\n" +
+	            "\r" + "3.Exit" + "\n" +
+	            "\r" + "*****GG_Bank*****" + "\n" +
+	            "\r" + "Contact Us-" + "\n" +
+	            "\r" + "Gaurav Bhalerao -77210 02054" +
+	            "\r" + "Gagan Matkar    -70403 99250" );
+	}
+
+	public static void existAccountMenu(){
+	    System.out.println("What would you like to do?" + "\n" +
+	                "\r" + "1.Acc Info" + "\n" +
+	                "\r" + "2.Deposit Money" + "\n" +
+	                "\r" + "3.Withdrawal Money" + "\n" +
+	                "\r" + "4.Money Transferring" + "\n" +
+	                "\r" + "5.Exit");
+	}
+
+
+}
